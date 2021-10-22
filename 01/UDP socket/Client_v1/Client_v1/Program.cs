814dfd65ac59dbade496ec1820b441e79fc34acc
4501104220 <thangtrantt@hotmail.com>
2021-10-22 10:23:18 +0700

    add: complete project

---
+++ 01/UDP socket/Client_v1/Client_v1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Text; 
+using System.Net; 
+using System.Net.Sockets; 
+
+namespace Client_v1
+
+{
+    class Program 
+    { 
+        static void Main(string[] args) { 
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2008); 
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); 
+            
+            String s = "Chao ban den voi Client"; 
+            byte[] data = new byte[1024]; 
+            data = Encoding.ASCII.GetBytes(s); 
+            client.SendTo(data, iep); 
+
+            EndPoint remote = (EndPoint)iep;
+            data = new byte[1024]; 
+            int recv = client.ReceiveFrom(data, ref remote); 
+            s = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("Nhan ve tu Server: {0}",s);
+
+            while (true) 
+            { 
+                s = Console.ReadLine(); 
+                data=new byte[1024]; 
+                data = Encoding.ASCII.GetBytes(s); 
+                client.SendTo(data, remote); 
+
+                if (s.ToUpper().Equals("QUIT")) break; 
+                data = new byte[1024]; 
+                recv = client.ReceiveFrom(data, ref remote); 
+               
+                s = Encoding.ASCII.GetString(data, 0, recv); 
+                Console.WriteLine("Server nhan duoc: {0} ", s);
+            } 
+            client.Close(); 
+        } 
+    } 
+}
---
+++ 01/UDP socket/Server_v1/Server_v1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_v1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2008);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(iep);
+            Console.WriteLine("Cho ket noi tu Client ");
+
+            // Tao ra mot Endpoint tu xa de nhan du lieu ve 
+            IPEndPoint RemoteEp = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remote = (EndPoint)RemoteEp;
+
+            byte[] data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref remote);
+            string s = Encoding.ASCII.GetString(data, 0, recv);
+
+            Console.WriteLine("Nhan ve tu Client: {0}", s);
+            data = Encoding.ASCII.GetBytes(" Chao ban den voi Server");
+            server.SendTo(data, remote);
+
+            while (true)
+            {
+                data = new byte[1024];
+                recv = server.ReceiveFrom(data, ref remote);
+
+                // Chuyen mang byte Data thanh chuoi va in ra man hinh 
+                s = Encoding.ASCII.GetString(data, 0, recv);           
+                Console.WriteLine("Client gui len: {0} ", s);
+
+                // Neu chuoi nhan duoc la Quit thi thoat
+                if (s.ToUpper().Equals("QUIT")) break;
+
+                
+                // Gui tra lai cho client chuoi s 
+                data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(s);
+                server.SendTo(data, 0, data.Length, SocketFlags.None, remote);
+            }
+            server.Close();
+        }
+    }
+}
